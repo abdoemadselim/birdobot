609e5819e168f5e3da1be956df6b19319b60a8f1
abdoemadselim <abdoemadselim11@gmail.com>
2026-01-17 15:46:40 +0200

    add features

--- src/app/api/telegram/route.ts
+++ src/app/api/telegram/route.ts
@@ -11,11 +11,9 @@ export const POST = async (request: NextRequest) => {
     try {
         const body = await request.json() as { message: TelegramBot.Message, chat: TelegramBot.Chat }
 
-        console.log(body)
-
         //1- Check Token From The Body
         const messageText = body.message.text
-        if (!messageText) return NextResponse.json({ message: "invalid token" }, { status: 401 })
+        if (!messageText || !messageText.startsWith("/start")) return NextResponse.json({ message: "invalid token" }, { status: 401 })
 
         const token = messageText.split(" ")[1]?.trim();
 
