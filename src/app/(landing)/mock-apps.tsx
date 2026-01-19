6894cc4220ca4873aec754ab646701a9ac2980ed
abdoemadselim <abdoemadselim11@gmail.com>
2026-01-19 09:41:23 +0200

    add features

--- src/app/(landing)/mock-apps.tsx
+++ src/app/(landing)/mock-apps.tsx
@@ -44,9 +44,9 @@ export default function MockApps() {
                 <div className="bg-brand-700 absolute bottom-24 top-24 inset-x-0"></div>
                 <div className="relative min-h-[900px]">
                     <div
-                        className="rounded-xl bg-gray-900/5 ring-1 ring-inset hover:ring-gray-900/40 max-xl:min-w-[90%] mx-auto
-                     ring-gray-900/10 peer/a lg:rounded-2xl lg:p-4 lg:-m-4 p-2 -m-2 absolute 2xl:left-[20%] xl:left-[10%] max-xl:left-1/2 max-xl:-translate-x-1/2 
-                      xl:opacity-60 hover:xl:opacity-100  hover:xl:-translate-y-[calc(50%+60px)] transition-all
+                        className="rounded-xl bg-gray-900/5 ring-1 ring-inset hover:ring-gray-900/40 max-lg:min-w-[90%] mx-auto
+                     ring-gray-900/10 peer/a lg:rounded-2xl lg:p-4 lg:-m-4 p-2 -m-2 absolute 2xl:left-[20%] xl:left-[10%] max-xl:left-1/2 max-xl:-translate-x-1/2 max-xl:-translate-y-[calc(50%-20px)]
+                      xl:opacity-60 hover:xl:opacity-100  hover:xl:-translate-y-[calc(50%+60px)] transition-all 
                       duration-400 hover:z-[50] z-10 top-1/2 -translate-y-1/2 w-fit
                       "
                     >
@@ -96,7 +96,7 @@ export default function MockApps() {
                         </MockDiscordUI>
                     </div>
 
-                    <div className="rounded-xl bg-gray-900/5 ring-1 ring-inset hover:ring-gray-900/40 max-xl:min-w-[92%] mx-auto
+                    <div className="rounded-xl bg-gray-900/5 ring-1 ring-inset hover:ring-gray-900/40 max-lg:min-w-[92%] mx-auto
                     max-xl:top-[calc(50%+80px)] max-xl:shadow-2xl 
                     ring-gray-900/10 peer/b lg:rounded-2xl lg:p-4 lg:-m-4 p-2 -m-2 absolute 2xl:left-[50%] xl:left-[40%] max-xl:left-1/2 max-xl:-translate-x-1/2
                      xl:opacity-60 hover:opacity-100 transition-all duration-400 hover:z-50 z-20
@@ -107,7 +107,7 @@ export default function MockApps() {
                         </MockSlackUI>
                     </div>
 
-                    <div className="rounded-xl bg-gray-900/5 ring-1 ring-inset ring-gray-900/40 peer-hover/a:ring-gray-900/10 max-xl:min-w-[94%] mx-auto
+                    <div className="rounded-xl bg-gray-900/5 ring-1 ring-inset ring-gray-900/40 peer-hover/a:ring-gray-900/10 max-lg:min-w-[94%] mx-auto
                      max-xl:top-170 peer-hover/b:ring-gray-900/10 peer-hover/b:-translate-y-[50%] peer-hover/a:-translate-y-[50%]  max-xl:left-1/2 max-xl:-translate-x-1/2 
                       lg:rounded-2xl lg:p-4 peer-hover/a:opacity-60 peer-hover/b:opacity-60 lg:-m-4 p-2 -m-2 absolute
                       2xl:left-[35%] xl:left-[25%] opacity-100 hover:opacity-100  -translate-y-[calc(50%+60px)] transition-all
