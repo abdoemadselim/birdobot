0d5e550ba5858962d9bbd116415840ed46f0943f
abdoemadselim <abdoemadselim11@gmail.com>
2026-01-26 19:06:41 +0200

    add features

--- src/app/(landing)/service-preview.tsx
+++ src/app/(landing)/service-preview.tsx
@@ -7,7 +7,7 @@ export default function ServicePreview() {
   return (
     <section className="sm:py-32 pt-44 pb-22" >
       <MaxWidthWrapper className="flex flex-col items-center justify-center">
-        <div className="pb-20 flex flex-col justify-center items-center">
+        <div className="pb-10 flex flex-col justify-center items-center">
           <Heading headingType="h2" className="text-center text-brand-700 font-medium bg-brand-100/50 rounded-lg px-4">See how it works?</Heading>
           <p className="text-zinc-500 pt-2">Make your decision right now!</p>
         </div>
